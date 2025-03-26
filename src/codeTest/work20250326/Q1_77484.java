1a13e90f83b84b5884126feb1768edd79a7455c8
kwonh <kwonh@DESKTOP-I3918AT>
2025-03-26 20:28:11 +0900

    20250326 study add

---
+++ src/codeTest/work20250326/Q1_77484.java
@@ -0,0 +1,24 @@
+package codeTest.work20250326;
+
+public class Q1_77484 {
+    public int[] solution(int[] lottos, int[] win_nums) {
+        int[] answer = new int[2];
+        int zero = 0;
+        int same = 0;
+        for(int n : lottos) {
+            if(n == 0) {
+                zero++;
+                continue;
+            }
+            for(int win : win_nums) {
+                if(n == win) {
+                    same++;
+                    break;
+                }
+            }
+        }
+        answer[0] = Math.min(7 - zero - same, 6);
+        answer[1] = Math.min(7 - same, 6);
+        return answer;
+    }
+}

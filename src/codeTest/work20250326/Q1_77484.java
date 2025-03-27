0fcda42b055693d5d32936fc10441fb13be3d4d0
kwonh <kwonh@DESKTOP-I3918AT>
2025-03-27 17:24:38 +0900

    20250327 codetest edit

--- src/codeTest/work20250326/Q1_77484.java
+++ src/codeTest/work20250326/Q1_77484.java
@@ -3,22 +3,22 @@ package codeTest.work20250326;
 public class Q1_77484 {
     public int[] solution(int[] lottos, int[] win_nums) {
         int[] answer = new int[2];
-        int zero = 0;
-        int same = 0;
+        int z = 0;
+        int s = 0;
         for(int n : lottos) {
             if(n == 0) {
-                zero++;
+                z++;
                 continue;
             }
             for(int win : win_nums) {
                 if(n == win) {
-                    same++;
+                    s++;
                     break;
                 }
             }
         }
-        answer[0] = Math.min(7 - zero - same, 6);
-        answer[1] = Math.min(7 - same, 6);
+        answer[0] = Math.min(7 - z - s, 6);
+        answer[1] = Math.min(7 - s, 6);
         return answer;
     }
 }
--- src/codeTest/work20250326/Q2_340199.java
+++ src/codeTest/work20250326/Q2_340199.java
@@ -3,28 +3,24 @@ package codeTest.work20250326;
 public class Q2_340199 {
     public int solution(int[] wallet, int[] bill) {
         int answer = 0;
-        int w_min = Math.min(wallet[0],wallet[1]);
-        int w_max = Math.max(wallet[0],wallet[1]);
-        int b_min = Math.min(bill[0],bill[1]);
-        int b_max = Math.max(bill[0],bill[1]);
 
-        while(true){
-            if(w_min < b_min || w_max < b_max){
-                b_max /= 2;
-                answer++;
-            }
+        int walletMin = Math.min(wallet[0], wallet[1]);
+        int walletMax = Math.max(wallet[0], wallet[1]);
+        int billMin = Math.min(bill[0], bill[1]);
+        int billMax = Math.max(bill[0], bill[1]);
 
-            if(b_max < b_min){
-                int temp = b_max;
-                b_max = b_min;
-                b_min = temp;
-            }
+        while (billMin > walletMin || billMax > walletMax) {
+            billMax /= 2;
+            answer++;
 
-            if(b_max <= w_max && b_min <= w_min){
-                break;
+            if (billMax < billMin) {
+                int temp = billMax;
+                billMax = billMin;
+                billMin = temp;
             }
         }
 
         return answer;
     }
 }
+

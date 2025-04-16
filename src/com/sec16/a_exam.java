d16735a71359c082314538d156634b2881ccc04f
kwonh <kwonh@DESKTOP-I3918AT>
2025-04-17 08:56:54 +0900

    20250416 study add

--- src/com/sec16/a_exam.java
+++ src/com/sec16/a_exam.java
@@ -3,15 +3,36 @@ package com.sec16;
 public class a_exam implements Runnable{
 
 	public static void main(String[] args) {
+		System.out.println("========main start========");
 		Thread t1 = new Thread(new a_exam(), "고양이");
 		Thread t2 = new Thread(new a_exam(), "강아지");
 		t1.start();
 		t2.start();
+		
+		System.out.println("========main mid========");
+		try {
+			t1.join();
+			t2.join();
+		}catch(InterruptedException i) {
+			System.out.println(i);
+		}
+		System.out.println("========main end========");
+		
+		//new a_exam().run(); -> 메소드 단위 실행
 	}
 
 	@Override
 	public void run() {
-		System.out.println("스레드");
+		for(int i = 0 ; i <= 10 ; i ++) {
+			System.out.println("스레드");
+			
+			try {
+				Thread.sleep(500);
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
 		
 	}
 }
